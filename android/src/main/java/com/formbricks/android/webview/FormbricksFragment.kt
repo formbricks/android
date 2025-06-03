e0e27bef67974c1f76198cb53623b2ff0eb561c7
pandeymangg <anshuman.pandey9999@gmail.com>
2025-06-03 15:15:39 +0700

    fixes log

--- android/src/main/java/com/formbricks/android/webview/FormbricksFragment.kt
+++ android/src/main/java/com/formbricks/android/webview/FormbricksFragment.kt
@@ -174,11 +174,6 @@ class FormbricksFragment : BottomSheetDialogFragment() {
             it.webChromeClient = object : WebChromeClient() {
                 override fun onConsoleMessage(consoleMessage: ConsoleMessage?): Boolean {
                     consoleMessage?.let { cm ->
-                        if (cm.messageLevel() == ConsoleMessage.MessageLevel.ERROR) {
-                            val error = SDKError.surveyDisplayFetchError
-                            Logger.e(error)
-                            safeDismiss()
-                        }
                         val log = "[CONSOLE:${cm.messageLevel()}] \"${cm.message()}\", source: ${cm.sourceId()} (${cm.lineNumber()})"
                         Logger.d(log)
                     }
